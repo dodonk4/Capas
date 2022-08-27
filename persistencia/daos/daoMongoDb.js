bb1ecee5a71127e4be5d0f9aac6e98c3efb96624
dodonk4 <50113883+dodonk4@users.noreply.github.com>
2022-08-27 14:38:32 -0300

    .

---
+++ persistencia/daos/daoMongoDb.js
@@ -0,0 +1,13 @@
+import contenedorMongoDb from "../contenedores/contenedorMongoDb.js";
+
+class DaoMongoDb extends contenedorMongoDb {
+
+    constructor() {
+        super('usuariosRegistrados', {
+            nombre: { type: String, required: true },
+            contraseña: { type: String, required: true }
+        })
+    }
+}
+
+export default DaoMongoDb;
